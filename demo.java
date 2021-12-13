1000af1c3d3a49642fb615ec98134e8655f1a4d7
mayur-patankar <patankarm06@gmail.com>
2021-12-13 12:27:44 +0530

    first add

--- demo.java
+++ demo.java
@@ -1,9 +1,8 @@
 class demo
 {
-public static void main(String args[]
-		{
-			System.out.println("Hello world");
-			System.out.println("modification");
-			System.out.println("Third mod");
-		}
+
+public static void main (String args[])
+{
+	System.out.println("Hello");
+}
 }
